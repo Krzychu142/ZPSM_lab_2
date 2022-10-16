5b8a61ec5cf69b7325b99d6d536e3cb6727af8b9
Krzysztof Radzięta <kustosz142@gmai.com>
2022-10-16 19:56:00 +0200

    comment to second exercise

--- lab_2/app.js
+++ lab_2/app.js
@@ -6,3 +6,5 @@ console.log(_.mean(testArray));
 
 console.log(_.min(testArray));
 console.log(_.max(testArray));
+
+// in second exercise my branch is called "develop" insted of "master", sorry for that, I didn't see this order and like always I made develop branch as default
